917a0a453af3e040b788d57171a0d7ae7a027c7e
Garrett <garrettswink421@gmail.com>
2023-10-12 12:25:03 -0400

    userControl

---
+++ controllers/userControl.js
@@ -0,0 +1,77 @@
+const { User } = require('../models');
+
+const userControl = {
+
+    getAllUsers(req, res) {
+        User.find({})
+        .then(userData => res.json(userData))
+        .catch(err => res.status(500).json(err));
+    },
+
+    getUserByID(req, res) {
+        User.findByID(req.params.userID)
+        .then(userData => res.json(userData))
+        .catch(err => res.status(500).json(err))
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+        .then(userData => res.json(userData))
+        .catch(err => res.status(500).json(err));
+    },
+
+    updateUserById(req, res) {
+        User.findByIDAndUpdate(req.params.id, req.body, { new: true })
+          .then(userData => {
+            if (!userData) {
+              return res.status(404).json({ message: 'User not found' });
+            }
+            res.json(userData);
+          })
+          .catch(err => res.status(500).json(err));
+      },
+
+      deleteUserById(req, res) {
+        User.findByIdAndDelete(req.params.id)
+        .then(userData => {
+            if (!userData) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            res.json({ message: 'User deleted'});
+        })
+        .catch(err => res.status(500).json(err));
+      },
+
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userID },
+            { $addToSet: { friends: req.body.friendID } },
+            { runValidators: true, new: true }
+        )
+            .then((user) => {
+                if (!user) {
+                    return res.status(404).json({ message: 'User ID not found' })
+                } else {
+                return res.json(user)}
+            })
+            .catch((err) => res.status(500).json(err));
+    },
+
+      deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userID },
+            { $pull: { friends: req.params.friendID } },
+            { new: true }
+        )
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({ message: 'User Id note found'})
+            } else {
+                return res.json(user)
+            }
+      })
+        .catch((err) => res.status(500).json(err));
+      }
+    };
+
+    module.exports = userControl;
